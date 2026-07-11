5e6552f719cd5393052fbcb5c41f7cbe769a8760
mdgufran <brothersags363@gmail.com>
2026-07-11 12:30:45 +0530

    Fix Render deployment

--- backend/server.js
+++ backend/server.js
@@ -383,7 +383,10 @@ await userRef.update({
 );
 
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
